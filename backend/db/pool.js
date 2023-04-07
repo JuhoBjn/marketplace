20e390754d3c3f2d679cf9623c1dcb3c29367c06
Juho Björkman <juho.bjorkman@tuni.fi>
2023-04-07 22:24:10 +0300

    Create connection pool

---
+++ backend/db/pool.js
@@ -0,0 +1,11 @@
+const mysql2 = require('mysql2')
+require('dotenv').config()
+
+const pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+})
+
+module.exports = pool
