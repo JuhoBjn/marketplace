339e8796c31e589e2d69d4d62dc0880e77c5fa09
Juho Björkman <juho.bjorkman@tuni.fi>
2023-04-07 22:23:52 +0300

    Init backend

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+const corsOptions = {
+  origin: 'localhost:5173',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.get('/pulsecheck', (req, res) => {
+  res.send('Pulse OK.')
+})
