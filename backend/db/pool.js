45b98b08b13e38edff32baba9c8f6edde9232689
Juho Björkman <juho.bjorkman@tuni.fi>
2023-04-09 17:58:08 +0300

    Add database to pool

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const pool = mysql2.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD
 })
