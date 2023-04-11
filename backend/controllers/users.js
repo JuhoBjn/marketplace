c2b83b8b03bbddadfec6b9d7724fc3ef3efb48a8
Juho Björkman <juho.bjorkman@tuni.fi>
2023-04-11 14:47:59 +0300

    Add api endpoint for user login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -49,43 +49,104 @@ const signup = async (req, res) => {
       if (err) {
         throw err
       }
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
+      bcrypt.hash(newUser.password, salt, async (err, hash) => {
         if (err) {
           throw err
         }
         newUser.password = hash
+
+        // Generate uuid and write new user to database.
+        newUser.id = genUuid()
+        const response = await userModels.create(newUser)
+        if (response.affectedRows === 0) {
+          throw new Error('Failed to write new user into database.')
+        }
+
+        // Generate token and respond to client.
+        const payload = {
+          id: newUser.id,
+          email: newUser.email
+        }
+        jwt.sign(
+          payload,
+          process.env.JWT_KEY,
+          { expiresIn: '1h' },
+          (err, token) => {
+            if (err) throw err
+            payload.token = token
+            res.status(201).send(payload)
+          }
+        )
       })
     })
-
-    // Generate uuid and write new user to database.
-    newUser.id = genUuid()
-    const response = await userModels.create(newUser)
-    if (response.affectedRows === 0) {
-      throw new Error('Failed to write new user into database.')
-    }
-
-    // Generate token and respond to client.
-    const payload = {
-      id: newUser.id,
-      email: newUser.email
-    }
-    jwt.sign(
-      payload,
-      process.env.JWT_KEY,
-      { expiresIn: '1h' },
-      (err, token) => {
-        if (err) {
-          console.log(err)
-          throw err
-        }
-        payload.token = token
-        res.status(201).send(payload)
-      }
-    )
   } catch (err) {
     console.log(err)
     res.status(500).send('Failed to create new user. Please try again.')
   }
 }
 
-module.exports = { signup }
+const login = async (req, res) => {
+  // Validate given input.
+  const validationSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(8).required()
+  })
+
+  const userCredentials = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  try {
+    const { error } = await validationSchema.validateAsync(userCredentials)
+    if (error) throw error
+  } catch (err) {
+    res.status(400).send(err.details[0].message)
+    return
+  }
+
+  // Check if the user exists.
+  const user = await userModels.findByEmail(userCredentials.email)
+  if (user.length === 0) {
+    res.status(401).send('Could not find user for given email')
+    return
+  }
+
+  try {
+    // Check password.
+    const match = await bcrypt.compare(
+      userCredentials.password,
+      user[0].password
+    )
+
+    if (!match) {
+      res
+        .status(401)
+        .send(
+          'Invalid credentials. Please check email and password and try again.'
+        )
+      return
+    }
+
+    // User credentials are correct. Generate token and respond to client.
+    const payload = {
+      id: user[0].id,
+      email: user[0].email
+    }
+    jwt.sign(
+      payload,
+      process.env.JWT_KEY,
+      { expiresIn: '1h' },
+      (err, token) => {
+        if (err) throw err
+        payload.token = token
+        res.send(payload)
+      }
+    )
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('Failed to log in user. Please try again.')
+  }
+}
+
+module.exports = { signup, login }
--- backend/paths/users.js
+++ backend/paths/users.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const { signup } = require('../controllers/users')
+const { signup, login } = require('../controllers/users')
 
 router.post('/signup', signup)
+router.post('/login', login)
 
 module.exports = router
