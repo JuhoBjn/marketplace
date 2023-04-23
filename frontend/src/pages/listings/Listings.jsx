d5aba873c8ff4491108632f21e28494a716e5ce1
Juho Björkman <juho.bjorkman@tuni.fi>
2023-04-23 22:52:18 +0300

    Add listings layout and style

--- frontend/src/pages/listings/Listings.jsx
+++ frontend/src/pages/listings/Listings.jsx
@@ -1,9 +1,16 @@
+import Card from "../../components/card/Card";
+
 import "./Listings.css";
 
 const Listings = () => {
   return (
-    <div className="listings-page center">
-      <h1>Listings page</h1>
+    <div className='listings-page center'>
+      <div className="listings-header">
+        <button>New listing</button>
+      </div>
+      <div className='listings-container'>
+        <Card />
+      </div>
     </div>
   );
 };
