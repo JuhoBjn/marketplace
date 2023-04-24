21fa21fc4013271cff94cce81a2291bd040be85c
Juho Björkman <juho.bjorkman@tuni.fi>
2023-04-24 19:03:13 +0300

    Add error checking to API communication functions

--- frontend/src/utils/ListingsAPI.js
+++ frontend/src/utils/ListingsAPI.js
@@ -1,24 +1,37 @@
 const fetchAll = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/listings`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  });
-  return response.json();
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/listings`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    return response.json();
+  } catch (err) {
+    console.log("Something went wrong querying the backend API.");
+    return "No listings found";
+  }
 };
 
 const fetchOwn = async (userId) => {
-  const response = await fetch(
-    `${import.meta.env.VITE_API_URL}/api/listings/${userId}`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  );
-  return response.json();
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/listings/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    return response.json();
+  } catch (err) {
+    console.log("Something went wrong querying the backend API.");
+    return "No listings found";
+  }
 };
 
 export { fetchAll, fetchOwn };
