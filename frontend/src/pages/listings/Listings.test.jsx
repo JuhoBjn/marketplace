c5ac7c24767ca42f9ecc16e5eee43fe4a709e5c2
Juho Björkman <juho.bjorkman@tuni.fi>
2023-04-25 02:15:07 +0300

    Update tests for listings and own listings pages

--- frontend/src/pages/listings/Listings.test.jsx
+++ frontend/src/pages/listings/Listings.test.jsx
@@ -2,13 +2,36 @@ import { render, screen } from "@testing-library/react";
 
 import Listings from "./Listings";
 
+import { AuthContext } from "../../utils/AuthContext";
+
 describe("The listings page", () => {
-  it("should contain button to add new listing", () => {
-    render(<Listings />);
+  it("should contain button to add new listing when logged in", () => {
+    render(
+      <AuthContext.Provider
+        value={{
+          id: "6c189af9-5168-49db-823b-34c3738d31c4",
+          firstname: "Tommy",
+          lastname: "Test",
+          email: "tommy@test.com",
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZjMTg5YWY5LTUxNjgtNDlkYi04MjNiLTM0YzM3MzhkMzFjNCIsImZpcnN0bmFtZSI6IlRvbW15IiwibGFzdG5hbWUiOiJUZXN0IiwiZW1haWwiOiJ0b21teUB0ZXN0LmNvbSIsImlhdCI6MTY4MjM3NzcxNywiZXhwIjoxNjgyMzgxMzE3fQ.GPPKL04-B5Z9WJd1CvLEMSY7lrfeNqjXFaX1GX5kMhI",
+        }}
+      >
+        <Listings />
+      </AuthContext.Provider>
+    );
 
     expect(
       screen.getByRole("button", { name: "New listing" })
     ).toBeInTheDocument();
+  });
+
+  it("should not contain a button to add a new listing when not logged in", () => {
+    render(<Listings />);
+
+    expect(
+      screen.queryByRole("button", { name: "New listing" })
+    ).not.toBeInTheDocument();
   });
 
   it("should have Listings container", () => {
